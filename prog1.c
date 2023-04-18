f6e2b6cbf1c4f310f833762db99ea505a2cb9c67
ajaysinh <ajay.dabhi@spiculestech.com>
2023-04-18 17:59:06 +0530

    change in program

--- prog1.c
+++ prog1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include "mymath.h"
 int main(){
    double x, y;
    printf("Enter first number: ");
    scanf("%lf",&x);
    printf("Enter second number: ");
    scanf("%lf",&y);
-   double ans = x + y ;
+   double ans = myadd(x, y) ;
    printf("a + b = %7.2lf\n",ans);
    return 0;
 }
